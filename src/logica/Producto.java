7b53fdc75b23bb9b84973afe89b9a1aa28d4ee60
Diego <pito.rodriguez05@gmail.com>
2026-05-13 23:25:53 -0600

    clase producto

--- src/logica/Producto.java
+++ src/logica/Producto.java
@@ -8,3 +8,48 @@ public class Producto {
     private float existencias;
     private String unidad;
     private float precio;
+
+    public Producto(String nombre, float existencias, String unidad, float precio) {
+        this.codigo = consecutivo++;
+        this.nombre = nombre;
+        this.existencias = existencias;
+        this.unidad = unidad;
+        this.precio = precio;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public float getExistencias() {
+        return existencias;
+    }
+
+    public String getUnidad() {
+        return unidad;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setExistencias(float existencias) {
+        this.existencias = existencias;
+    }
+
+    public void setUnidad(String unidad) {
+        this.unidad = unidad;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+}
