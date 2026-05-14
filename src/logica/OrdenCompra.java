095811ce4b4d797dd38e3b517b0d21f0e6502985
Diego <pito.rodriguez05@gmail.com>
2026-05-13 23:28:26 -0600

    clase orden compra

--- src/logica/OrdenCompra.java
+++ src/logica/OrdenCompra.java
@@ -12,3 +12,81 @@ public class OrdenCompra {
     private String estado;
     private Cliente cliente;
     private List<LineaOrden> lineas;
+
+    public OrdenCompra(Cliente cliente) {
+        this.numero = consecutivo++;
+        this.fecha = LocalDate.now();
+        this.estado = "Iniciada";
+        this.cliente = cliente;
+        this.lineas = new ArrayList<>();
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public List<LineaOrden> obtenerLineas() {
+        return lineas;
+    }
+
+    public void agregarLinea(Producto producto, float cantidad) {
+        LineaOrden nuevaLinea = new LineaOrden(producto, cantidad);
+        lineas.add(nuevaLinea);
+    }
+
+    public void actualizarLinea(int numeroLinea, Producto producto, float cantidad) {
+        if (numeroLinea < 0 || numeroLinea >= lineas.size()) {
+            throw new IllegalArgumentException("El numero de linea no es valido.");
+        }
+
+        LineaOrden linea = lineas.get(numeroLinea);
+        linea.setProducto(producto);
+        linea.setCantidad(cantidad);
+    }
+
+    public void borrarLinea(int numeroLinea) {
+        if (numeroLinea < 0 || numeroLinea >= lineas.size()) {
+            throw new IllegalArgumentException("El numero de linea no es valido.");
+        }
+
+        lineas.remove(numeroLinea);
+    }
+
+    public void ponerPendiente() {
+        this.estado = "Pendiente";
+    }
+
+    public void ponerTerminada() {
+        this.estado = "Terminada";
+    }
+
+    public float getMonto() {
+        float monto = 0;
+
+        for (LineaOrden linea : lineas) {
+            monto += linea.getCosto();
+        }
+
+        return monto;
+    }
+
+    public float getImpuesto() {
+        return getMonto() * 0.13f;
+    }
+
+    public float getMontoTotal() {
+        return getMonto() + getImpuesto();
+    }
+}
