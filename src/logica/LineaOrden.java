384fb3317baccfee238de62ed030dc790e970c3d
Diego <pito.rodriguez05@gmail.com>
2026-05-13 23:27:28 -0600

    clase lineaOrden

--- src/logica/LineaOrden.java
+++ src/logica/LineaOrden.java
@@ -3,3 +3,29 @@ package logica;
 public class LineaOrden {
     private Producto producto;
     private float cantidad;
+
+    public LineaOrden(Producto producto, float cantidad) {
+        this.producto = producto;
+        this.cantidad = cantidad;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public float getCantidad() {
+        return cantidad;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public void setCantidad(float cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public float getCosto() {
+        return cantidad * producto.getPrecio();
+    }
+}
